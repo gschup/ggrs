ebc9e611c71e5aae5a0451d56f38bf331481811f
Georg Schuppe <georg.schuppe@gmail.com>
2021-06-08 21:14:59 +0200

    test: expanded p2p synchronization test

--- ggrs/tests/test_p2p_session.rs
+++ ggrs/tests/test_p2p_session.rs
@@ -1,4 +1,4 @@
-use ggrs::GGRSSession;
+use ggrs::{GGRSSession, SessionState};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 use serial_test::serial;
@@ -47,4 +47,37 @@ fn test_disconnect_player() {
     assert!(sess.disconnect_player(0).is_err()); // for now, local players cannot be disconnected
     assert!(sess.disconnect_player(1).is_ok());
     assert!(sess.disconnect_player(1).is_err()); // already disconnected
+}
+
+#[test]
+#[serial]
+fn test_synchronize_p2p_sessions() {
+    let mut stub1 = stubs::GameStub::new();
+    let mut stub2 = stubs::GameStub::new();
+    let mut sess1 = ggrs::start_p2p_session(2, std::mem::size_of::<u32>(), 7777).unwrap();
+    let mut sess2 = ggrs::start_p2p_session(2, std::mem::size_of::<u32>(), 8888).unwrap();
+    let addr1 = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 7777);
+    let addr2 = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8888);
+
+    assert!(sess1.current_state() == SessionState::Initializing);
+    assert!(sess2.current_state() == SessionState::Initializing);
+
+    assert!(sess1.add_player(ggrs::PlayerType::Local, 0).is_ok());
+    assert!(sess1.add_player(ggrs::PlayerType::Remote(addr2), 1).is_ok());
+    assert!(sess1.start_session().is_ok());
+
+    assert!(sess2.add_player(ggrs::PlayerType::Local, 1).is_ok());
+    assert!(sess2.add_player(ggrs::PlayerType::Remote(addr1), 0).is_ok());
+    assert!(sess2.start_session().is_ok());
+
+    assert!(sess1.current_state() == SessionState::Synchronizing);
+    assert!(sess2.current_state() == SessionState::Synchronizing);
+
+    for _ in 0..10 {
+        sess1.idle(&mut stub1);
+        sess2.idle(&mut stub2);
+    }
+
+    assert!(sess1.current_state() == SessionState::Running);
+    assert!(sess2.current_state() == SessionState::Running);
 }
