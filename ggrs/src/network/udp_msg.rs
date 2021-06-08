1877bd2b0de8918bec6354d85a0c65f033c3d6f4
Georg Schuppe <georg.schuppe@gmail.com>
2021-06-08 09:51:54 +0200

    refactor: structs inside message enum no longer anonymous

--- ggrs/src/network/udp_msg.rs
+++ ggrs/src/network/udp_msg.rs
@@ -2,20 +2,20 @@ use serde::{Deserialize, Serialize};
 
 use crate::{FrameNumber, NULL_FRAME};
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct MessageHeader {
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Default)]
+pub(crate) struct MessageHeader {
     pub magic: u16,
     pub sequence_number: u16,
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct ConnectionStatus {
+pub(crate) struct ConnectionStatus {
     pub disconnected: bool,
     pub last_frame: FrameNumber,
 }
 
-impl ConnectionStatus {
-    pub fn new() -> Self {
+impl Default for ConnectionStatus {
+    fn default() -> Self {
         Self {
             disconnected: false,
             last_frame: NULL_FRAME,
@@ -23,40 +23,83 @@ impl ConnectionStatus {
     }
 }
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Default)]
+pub(crate) struct SyncRequest {
+    pub header: MessageHeader,
+    pub random_request: u32, // please reply back with this random data
+    pub remote_magic: u16,
+    pub remote_endpoint: u8,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Default)]
+pub(crate) struct SyncReply {
+    pub header: MessageHeader,
+    pub random_reply: u32, // here's your random data back
+}
+
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub enum UdpMessage {
-    SyncRequest {
-        header: MessageHeader,
-        random_request: u32, // please reply back with this random data
-        remote_magic: u16,
-        remote_endpoint: u8,
-    },
-    SyncReply {
-        header: MessageHeader,
-        random_reply: u32, // here's your random data back
-    },
-    Input {
-        header: MessageHeader,
-        peer_connect_status: Vec<ConnectionStatus>,
-        start_frame: FrameNumber,
-        disconnect_requested: bool,
-        ack_frame: FrameNumber,
-        bits: Vec<u8>,
-    },
-    InputAck {
-        header: MessageHeader,
-        ack_frame: FrameNumber,
-    },
-    QualityReport {
-        header: MessageHeader,
-        frame_advantage: i8, // frame advantage of other player
-        ping: u128,
-    },
-    QualityReply {
-        header: MessageHeader,
-        pong: u128,
-    },
-    KeepAlive {
-        header: MessageHeader,
-    },
+pub(crate) struct Input {
+    pub header: MessageHeader,
+    pub peer_connect_status: Vec<ConnectionStatus>,
+    pub disconnect_requested: bool,
+    pub start_frame: FrameNumber,
+    pub ack_frame: FrameNumber,
+    pub bits: Vec<u8>,
+}
+
+impl Default for Input {
+    fn default() -> Self {
+        Self {
+            header: MessageHeader::default(),
+            peer_connect_status: Vec::new(),
+            disconnect_requested: false,
+            start_frame: NULL_FRAME,
+            ack_frame: NULL_FRAME,
+            bits: Vec::new(),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub(crate) struct InputAck {
+    pub header: MessageHeader,
+    pub ack_frame: FrameNumber,
+}
+
+impl Default for InputAck {
+    fn default() -> Self {
+        Self {
+            header: MessageHeader::default(),
+            ack_frame: NULL_FRAME,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Default)]
+pub(crate) struct QualityReport {
+    pub header: MessageHeader,
+    pub frame_advantage: i8, // frame advantage of other player
+    pub ping: u128,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Default)]
+pub(crate) struct QualityReply {
+    header: MessageHeader,
+    pong: u128,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Default)]
+pub(crate) struct KeepAlive {
+    header: MessageHeader,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub(crate) enum UdpMessage {
+    SyncRequest(SyncRequest),
+    SyncReply(SyncReply),
+    Input(Input),
+    InputAck(InputAck),
+    QualityReport(QualityReport),
+    QualityReply(QualityReply),
+    KeepAlive(KeepAlive),
 }
