1456a04ca3833cd6fd48abb8177ebefc84df8d68
Georg Schuppe <georg.schuppe@gmail.com>
2021-06-14 16:00:30 +0200

    refactor: InputBuffer is now bigger by a factor MAX_PLAYERS to accomodate for inputs being sent to the spectators

--- src/frame_info.rs
+++ src/frame_info.rs
@@ -1,12 +1,8 @@
-use crate::{FrameNumber, MAX_INPUT_BYTES, NULL_FRAME};
+use crate::{FrameNumber, MAX_INPUT_BYTES, MAX_PLAYERS, NULL_FRAME};
 
-pub type InputBuffer = [u8; MAX_INPUT_BYTES];
-
-pub(crate) const BLANK_FRAME: FrameInfo = FrameInfo {
-    frame: NULL_FRAME,
-    state: BLANK_STATE,
-    input: BLANK_INPUT,
-};
+/// The input buffer used to save the bytes from a player input. It is bigger than necessary by a factor `MAX_PLAYERS` to allow the same type of buffers to be used to transmit
+/// player inputs for all players to the spectators. This definitely isn't optimal and might be changed later.
+pub type InputBuffer = [u8; MAX_INPUT_BYTES * MAX_PLAYERS as usize];
 
 pub(crate) const BLANK_STATE: GameState = GameState {
     frame: NULL_FRAME,
@@ -16,17 +12,9 @@ pub(crate) const BLANK_STATE: GameState = GameState {
 
 pub const BLANK_INPUT: GameInput = GameInput {
     frame: NULL_FRAME,
-    buffer: [0; MAX_INPUT_BYTES],
+    buffer: [0; MAX_INPUT_BYTES * MAX_PLAYERS as usize],
     size: 0,
 };
-
-/// This struct holds a state and an input. It is intended that both the state and the input correspond to the same frame.
-#[derive(Debug, Clone, Default)]
-pub(crate) struct FrameInfo {
-    pub frame: FrameNumber,
-    pub state: GameState,
-    pub input: GameInput,
-}
 
 /// Represents a serialized game state of your game for a single frame. The buffer `buffer` holds your state, `frame` indicates the associated frame number
 /// and `checksum` can additionally be provided for use during a `SyncTestSession`. You are expected to return this during `save_game_state()` and use them during `load_game_state()`.
@@ -64,7 +52,7 @@ pub struct GameInput {
     pub frame: FrameNumber,
     // The input size per player
     pub size: usize,
-    /// The game input for a player in a single frame
+    /// An input buffer that will hold input data
     pub buffer: InputBuffer,
 }
 
@@ -80,11 +68,13 @@ impl GameInput {
             None => Self {
                 frame,
                 size,
-                buffer: [0; crate::MAX_INPUT_BYTES],
+                buffer: [0; MAX_INPUT_BYTES * MAX_PLAYERS as usize],
             },
         }
     }
+}
 
+impl GameInput {
     pub(crate) fn copy_input(&mut self, bytes: &[u8]) {
         assert!(bytes.len() == self.size);
         self.buffer[0..self.size].copy_from_slice(bytes);
