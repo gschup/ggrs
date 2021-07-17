a3c7ba0997a46fb3c9a94242b0e700283339907e
gschup <georg.schuppe@gmail.com>
2021-07-17 07:59:33 +0200

    chore: removed bevy stub

--- examples/bevy/bevy_synctest.rs
+++
@@ -1,8 +0,0 @@
-use bevy::prelude::*;
-
-fn main() {
-    App::build()
-        .add_plugins(DefaultPlugins)
-        .add_system(bevy::input::system::exit_on_esc_system.system())
-        .run();
-}
