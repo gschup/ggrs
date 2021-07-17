9d8ab20a1c2cec0f2d23d77394d15e9c99dffcc3
gschup <georg.schuppe@gmail.com>
2021-07-17 09:36:32 +0200

    test: test for check distance = 0

--- tests/test_synctest_session.rs
+++ tests/test_synctest_session.rs
@@ -30,6 +30,24 @@ fn test_start_synctest_session() {
 #[test]
 fn test_advance_frame() {
     let handle = 1;
+    let check_distance = 0;
+    let mut stub = stubs::GameStub::new();
+    let mut sess = ggrs::start_synctest_session(2, stubs::INPUT_SIZE, check_distance).unwrap();
+    assert!(sess.add_player(PlayerType::Local, handle).is_ok());
+    assert!(sess.start_session().is_ok());
+
+    for i in 0..200 {
+        let input: u32 = i;
+        let serialized_input = bincode::serialize(&input).unwrap();
+        let requests = sess.advance_frame(handle, &serialized_input).unwrap();
+        stub.handle_requests(requests);
+        assert_eq!(stub.gs.frame, i as i32 + 1); // frame should have advanced
+    }
+}
+
+#[test]
+fn test_advance_frame_with_rollbacks() {
+    let handle = 1;
     let check_distance = 7;
     let mut stub = stubs::GameStub::new();
     let mut sess = ggrs::start_synctest_session(2, stubs::INPUT_SIZE, check_distance).unwrap();
