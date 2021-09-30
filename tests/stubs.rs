685c315c19177b02b2e9ad71fa8b58e8db2e5c8e
gschup <georg.schuppe@gmail.com>
2021-09-30 11:34:58 +0200

    test: synctest with random checksum

--- tests/stubs.rs
+++ tests/stubs.rs
@@ -1,4 +1,5 @@
 use bincode;
+use rand::{prelude::ThreadRng, thread_rng, Rng};
 use serde::{Deserialize, Serialize};
 
 use ggrs::{Frame, GGRSRequest, GameInput, GameState, GameStateCell};
@@ -33,6 +34,50 @@ impl GameStub {
         let buffer = bincode::serialize(&self.gs).unwrap();
 
         cell.save(GameState::new(frame, Some(buffer), None));
+    }
+
+    fn load_game_state(&mut self, cell: GameStateCell) {
+        let game_state = cell.load();
+        self.gs = bincode::deserialize(&game_state.buffer.unwrap()).unwrap();
+    }
+
+    fn advance_frame(&mut self, inputs: Vec<GameInput>) {
+        self.gs.advance_frame(inputs);
+    }
+}
+
+pub struct RandomChecksumGameStub {
+    pub gs: GameStateStub,
+    rng: ThreadRng,
+}
+
+impl RandomChecksumGameStub {
+    #[allow(dead_code)]
+    pub fn new() -> RandomChecksumGameStub {
+        RandomChecksumGameStub {
+            gs: GameStateStub { frame: 0, state: 0 },
+            rng: thread_rng(),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn handle_requests(&mut self, requests: Vec<GGRSRequest>) {
+        for request in requests {
+            match request {
+                GGRSRequest::LoadGameState { cell } => self.load_game_state(cell),
+                GGRSRequest::SaveGameState { cell, frame } => self.save_game_state(cell, frame),
+                GGRSRequest::AdvanceFrame { inputs } => self.advance_frame(inputs),
+            }
+        }
+    }
+
+    fn save_game_state(&mut self, cell: GameStateCell, frame: Frame) {
+        assert_eq!(self.gs.frame, frame);
+        let buffer = bincode::serialize(&self.gs).unwrap();
+
+        let random_checksum: u64 = self.rng.gen();
+        println!("saving frame {} with checksum {}", frame, random_checksum);
+        cell.save(GameState::new(frame, Some(buffer), Some(random_checksum)));
     }
 
     fn load_game_state(&mut self, cell: GameStateCell) {
--- tests/test_synctest_session.rs
+++ tests/test_synctest_session.rs
@@ -43,3 +43,23 @@ fn test_advance_frames_with_delayed_input() {
         assert_eq!(stub.gs.frame, i as i32 + 1); // frame should have advanced
     }
 }
+
+#[test]
+#[should_panic]
+fn test_advance_frames_with_random_checksums() {
+    let handle = 1;
+    let check_distance = 2;
+    let mut stub = stubs::RandomChecksumGameStub::new();
+    let mut sess = SyncTestSession::new(2, stubs::INPUT_SIZE, check_distance).unwrap();
+    assert!(sess.set_frame_delay(2, handle).is_ok());
+
+    for i in 0..200 {
+        let input: u32 = i;
+        let mut serialized_input = Vec::new();
+        serialized_input.push(bincode::serialize(&input).unwrap());
+        serialized_input.push(bincode::serialize(&input).unwrap());
+        let requests = sess.advance_frame(&serialized_input).unwrap();
+        stub.handle_requests(requests);
+        assert_eq!(stub.gs.frame, i as i32 + 1); // frame should have advanced
+    }
+}
